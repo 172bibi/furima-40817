4710f00b8e6e06fbc5d471125a7b603342d5d568
172bibi <g84.h.g84ib@gmail.com>
2024-06-04 19:37:21 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates :lastname, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :firstname, presence: true, format: { with: VALID_NAME_REGEX }
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :lastnamekana, presence: true, format: { with: VALID_KATAKANA_REGEX }
+  validates :firstnamekana, presence: true, format: { with: VALID_KATAKANA_REGEX }
+  validates :birthday, presence: true
 end
