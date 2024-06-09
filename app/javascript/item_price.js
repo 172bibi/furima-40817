5b780b2b3846743345d6629fed3de97131637651
172bibi <g84.h.g84ib@gmail.com>
2024-06-09 20:33:25 +0900

    販売手数料・利益計算のJavaScript実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const fee = Math.floor(inputValue * 0.1);  // 手数料を計算
+    const profit = inputValue - fee;  // 利益を計算
+
+    const feeOutput = document.getElementById('add-tax-price');
+    const profitOutput = document.getElementById('profit');
+
+    feeOutput.innerText = fee;  // 手数料を表示
+    profitOutput.innerText = profit;  // 利益を表示
+  })
+});
