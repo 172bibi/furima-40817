fc453eb7b45a1c4b114b34fd36d89561f963b728
172bibi <g84.h.g84ib@gmail.com>
2024-06-12 14:23:01 +0900

    エラーメッセージの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :prohibit_purchase, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @orderform = OrderForm.new
   end
 
   def create
     @orderform = OrderForm.new(order_params)
     if @orderform.valid?
-     @orderform.save(params,current_user.id)
+     @orderform.save
      redirect_to root_path
     else
-     @item = Item.find(params[:item_id])
-     render :index
+     render :index, status: :unprocessable_entity
     end
   end
-      
+
   private
-    def order_params
-     params.require(:order_form).permit(:postcode, :region_id, :municipalities, :streetaddress, :buildingname, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id])
-    end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+   params.require(:order_form).permit(:postcode, :region_id, :municipalities, :streetaddress, :buildingname, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def prohibit_purchase
+    redirect_to root_path if current_user.id == @item.user_id
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -5,11 +5,11 @@ class OrderForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :municipalities
     validates :streetaddress
-    validates :phonenumber, format: { with: /\A\d{10,11}\z/, message: "is invalid. Don't include hyphen(-)"}
+    validates :phonenumber, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number and 10 or 11 digits"}
   end
 
   def save
